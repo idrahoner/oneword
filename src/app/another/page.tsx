7c1eb321cd22c85f66cdb0f42ea4ce7bebe23f14
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-15 17:42:17 +0300

    Add prefetch - false

--- src/app/another/page.tsx
+++ src/app/another/page.tsx
@@ -3,11 +3,12 @@ import Link from 'next/link';
 export default function Another() {
   return (
     <main>
-      <div className="flex flex-columb justify-center items-center w-full h-96 bg-blue-200">
+      <div className="flex flex-col justify-center items-center w-full h-96 bg-blue-200">
         <h1 className="text-2xl font-bold text-gray-900">Another page</h1>
         <Link
           href="/"
           className="text-2xl font-bold text-zinc-800 hover:text-orange-700 transition-colors duration-300"
+          prefetch={false}
         >
           Go Home
         </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-columb justify-center items-center w-full h-96 bg-blue-200">
+      <div className="flex flex-col justify-center items-center w-full h-96 bg-blue-200">
         <h1 className="text-2xl font-bold text-gray-900">Home page</h1>
         <Link
           href="/another"
           className="text-2xl font-bold text-zinc-800 hover:text-orange-700 transition-colors duration-300"
+          prefetch={false}
         >
           Go Home
         </Link>
