8d169a45d5775d25707b02461488fe076b64dffd
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-15 17:25:49 +0300

    Add links

--- src/app/another/page.tsx
+++ src/app/another/page.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+
 export default function Another() {
   return (
     <main>
-      <div className="w-full h-96 bg-blue-200">
-        <h1 className="text-gray-900">Another page</h1>
+      <div className="flex flex-columb justify-center items-center w-full h-96 bg-blue-200">
+        <h1 className="text-2xl font-bold text-gray-900">Another page</h1>
+        <Link
+          href="/"
+          className="text-2xl font-bold text-zinc-800 hover:text-orange-700 transition-colors duration-300"
+        >
+          Go Home
+        </Link>
       </div>
     </main>
   );
