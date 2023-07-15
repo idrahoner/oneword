008da8566b9a4c7d12b3ef4e52296d915dc9a5f8
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-15 17:18:21 +0300

    Fixed next config and create another page

---
+++ src/app/another/page.tsx
@@ -0,0 +1,9 @@
+export default function Another() {
+  return (
+    <main>
+      <div className="w-full h-96 bg-blue-200">
+        <h1 className="text-gray-900">Another page</h1>
+      </div>
+    </main>
+  );
+}
